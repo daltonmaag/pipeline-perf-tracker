dcb0c6eca385aaf46c95be0ca3fb31310cbb5eff
Simon Cozens <simon@simon-cozens.org>
2021-03-27 07:20:09 +0000

    Something seems to take ages with fontmake setup, find out what

--- runner.py
+++ runner.py
@@ -185,14 +185,7 @@ class ProjectVersion:
         cmd = "\n".join(["source bin/activate", *self.setup_script])
         print(f"    Running setup_script:")
         print(textwrap.indent(cmd, " " * 8))
-        subprocess.check_call(
-            cmd,
-            shell=True,
-            executable="/bin/bash",
-            cwd=project_path,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
+        subprocess.check_call(cmd, shell=True, executable="/bin/bash", cwd=project_path)
 
     @staticmethod
     def from_data(data):
